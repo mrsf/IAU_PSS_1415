80e254e99fa424cabb028adc329c666b3a237402
mrsf <marciosilva15@hotmail.com>
2014-11-19 23:18:51 +0000

    Adição da lib do dropbox e algum codigo da sua utilização

--- app/src/main/java/pt/ipleiria/estg/meicm/iaupss/estgparking/ParkingLotsActivity.java
+++ app/src/main/java/pt/ipleiria/estg/meicm/iaupss/estgparking/ParkingLotsActivity.java
@@ -1,14 +1,35 @@
 package pt.ipleiria.estg.meicm.iaupss.estgparking;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+import com.dropbox.sync.android.DbxAccount;
+import com.dropbox.sync.android.DbxAccountManager;
+import com.dropbox.sync.android.DbxDatastore;
+import com.dropbox.sync.android.DbxDatastoreManager;
+import com.dropbox.sync.android.DbxException;
+import com.dropbox.sync.android.DbxRecord;
+import com.dropbox.sync.android.DbxTable;
 
 
 public class ParkingLotsActivity extends ActionBarActivity {
+
+    private static final String APP_KEY = "avpur29bk1xsrye";
+    private static final String APP_SECRET = "1d94tetjd15mt1s";
+    static final int REQUEST_LINK_TO_DBX = 0;  // This value is up to you
+
+    /* Initializing managers vars */
+    private Button mLinkButton;
+    private DbxAccountManager mAccountManager;
+    private DbxDatastoreManager mDatastoreManager;
 
     private RecyclerView mRecyclerView;
     private RecyclerView.Adapter mAdapter;
@@ -18,6 +39,36 @@ public class ParkingLotsActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parking_lots);
+
+        // Set up the account manager
+        mAccountManager = DbxAccountManager.getInstance(getApplicationContext(), APP_KEY, APP_SECRET);
+
+        // Button to link to Dropbox
+        /*mLinkButton = (Button) findViewById(R.id.link_button);
+        mLinkButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mAccountManager.startLink((ActionBarActivity)ParkingLotsActivity.this, REQUEST_LINK_TO_DBX);
+            }
+        });*/
+
+        // Set up the datastore manager
+        if (mAccountManager.hasLinkedAccount()) {
+            try {
+                // Use Dropbox datastores
+                mDatastoreManager = DbxDatastoreManager.forAccount(mAccountManager.getLinkedAccount());
+                // Hide link button
+                mLinkButton.setVisibility(View.GONE);
+            } catch (DbxException.Unauthorized e) {
+                System.out.println("Account was unlinked remotely");
+            }
+        }
+        if (mDatastoreManager == null) {
+            // Account isn't linked yet, use local datastores
+            mDatastoreManager = DbxDatastoreManager.localManager(mAccountManager);
+            // Show link button
+            mLinkButton.setVisibility(View.VISIBLE);
+        }
 
         mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
 
@@ -32,6 +83,19 @@ public class ParkingLotsActivity extends ActionBarActivity {
         // specify an adapter (see also next example)
         mAdapter = new ParkingLotAdapter(null);//myDataset);
         mRecyclerView.setAdapter(mAdapter);
+
+                /* Manage DataStore Records*/
+        try {
+            mAccountManager.startLink((ActionBarActivity)ParkingLotsActivity.this, REQUEST_LINK_TO_DBX);
+            DbxDatastore datastore = mDatastoreManager.openDefaultDatastore();
+            DbxTable parkingLotsTable = datastore.getTable("ParkingLots");
+            DbxRecord parkingLotRecord = parkingLotsTable.insert().set("nome", "Lote 1").
+                    set("descricao", "Em frente ao edificio D").
+                    set("latitude", 32.00).set("longitude", -8.00);
+            datastore.sync();
+        } catch (DbxException e) {
+            e.printStackTrace();
+        }
     }
 
 
@@ -56,4 +120,28 @@ public class ParkingLotsActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_LINK_TO_DBX) {
+            if (resultCode == Activity.RESULT_OK) {
+                DbxAccount account = mAccountManager.getLinkedAccount();
+                try {
+                    // Migrate any local datastores to the linked account
+                    mDatastoreManager.migrateToAccount(account);
+                    // Now use Dropbox datastores
+                    mDatastoreManager = DbxDatastoreManager.forAccount(account);
+                    // Hide link button
+                    mLinkButton.setVisibility(View.GONE);
+                } catch (DbxException e) {
+                    e.printStackTrace();
+                }
+            } else {
+                // Link failed or was cancelled by the user
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data);
+        }
+    }
+
 }
