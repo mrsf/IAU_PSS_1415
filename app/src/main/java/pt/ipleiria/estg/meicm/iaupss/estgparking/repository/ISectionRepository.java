688c36b3433ce65fc88e96f45162b96a127264a0
Pedro Edra Sousa <pedroedrasousa@gmail.com>
2015-01-13 19:44:14 +0000

    Section repository interface modification

--- app/src/main/java/pt/ipleiria/estg/meicm/iaupss/estgparking/repository/ISectionRepository.java
+++ app/src/main/java/pt/ipleiria/estg/meicm/iaupss/estgparking/repository/ISectionRepository.java
@@ -7,4 +7,14 @@ import pt.ipleiria.estg.meicm.iaupss.estgparking.model.Section;
 public interface ISectionRepository {
 
     List<Section> fetchSections();
+
+    /**
+     * Increment the occupation counter of a parking section.
+     *
+     * @param lat
+     * @param lng
+     * @return true if latitude and longitude are inside a known section and a parking
+     * spot was marked as occupied, false otherwise.
+     */
+    boolean occupySection(String lat, String lng);
 }
